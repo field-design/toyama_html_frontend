96d91f9caf634be86e1046a748dfbeaaf5db1b85
Masahiko <masahiko.emura@gmail.com>
2017-04-06 19:23:03 +0900

    追加・修正内容反映

--- source/assets/js/common.js
+++ source/assets/js/common.js
@@ -79,3 +79,22 @@ $(function(){
 		}
 	});
 });
+
+// カンマ区切り
+$.fn.comma3 = function(){
+	return this.each(function(){
+		var num3string = (function(num){
+				var str = new String(num);
+				var n = "";
+				var count = 0;
+				for (var i=str.length-1; i>=0; i--){
+					n = str.charAt(i) + n;
+					count++;
+					if (((count % 3) == 0) && (i != 0)) n = ","+n;
+				}
+				return n;
+			})(this.innerHTML);
+		this.innerHTML = num3string;
+	});
+};
+$(".num3").comma3();
